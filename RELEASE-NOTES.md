e20b3288f986c4e6eab882aa12e4d833e57911be
Andrus Adamchik <andrus@objectstyle.com>
2016-03-16 19:55:33 -0400

    Upgrade to Bootique 0.15 #5

---
+++ RELEASE-NOTES.md
@@ -0,0 +1,10 @@
+## 0.10
+
+* #5 Upgrade to Bootique 0.15
+
+## 0.9:
+
+* #1 Upgrade Bootique to 0.12
+* #2 Move contribution API from JobBinder into static methods on JobModule
+* #3 Move JobMetadataBuilder inside JobMetadata
+* #4 Trigger: support for 'initialDelayMs' YAML parameter
--- src/test/java/com/nhl/bootique/job/runtime/JobModuleProviderIT.java
+++ src/test/java/com/nhl/bootique/job/runtime/JobModuleProviderIT.java
@@ -1,26 +1,13 @@
 package com.nhl.bootique.job.runtime;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.ServiceLoader;
-
 import org.junit.Test;
 
-import com.nhl.bootique.BQModuleProvider;
-import com.nhl.bootique.job.runtime.JobModuleProvider;
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 
 public class JobModuleProviderIT {
+	
 	@Test
 	public void testPresentInJar() {
-
-		boolean[] found = { false };
-
-		ServiceLoader.load(BQModuleProvider.class).forEach(p -> {
-			if (p instanceof JobModuleProvider) {
-				found[0] = true;
-			}
-		});
-
-		assertTrue(found[0]);
+		BQModuleProviderChecker.testPresentInJar(JobModuleProvider.class);
 	}
 }
