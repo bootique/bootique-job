d8958a16c37f7599827d57d0f72bcce9518f67ea
Andrus Adamchik <andrus@objectstyle.com>
2017-08-31 21:04:22 +0300

    Better error reporting for unregistered jobs #42
    * more trigger errors converted to BootiqueException; better message formatting

--- bootique-job/src/main/java/io/bootique/job/scheduler/DefaultScheduler.java
+++ bootique-job/src/main/java/io/bootique/job/scheduler/DefaultScheduler.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ScheduledFuture;
@@ -28,67 +27,67 @@ import java.util.stream.Collectors;
 
 public class DefaultScheduler implements Scheduler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(DefaultScheduler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultScheduler.class);
 
-	private TaskScheduler taskScheduler;
-	private RunnableJobFactory runnableJobFactory;
-	private JobRegistry jobRegistry;
-	private Collection<TriggerDescriptor> triggers;
-	private Collection<ScheduledJob> scheduledJobs;
+    private TaskScheduler taskScheduler;
+    private RunnableJobFactory runnableJobFactory;
+    private JobRegistry jobRegistry;
+    private Collection<TriggerDescriptor> triggers;
+    private Collection<ScheduledJob> scheduledJobs;
 
-	private AtomicBoolean started;
+    private AtomicBoolean started;
 
-	public DefaultScheduler(Collection<TriggerDescriptor> triggers,
-							TaskScheduler taskScheduler,
-							RunnableJobFactory runnableJobFactory,
-							JobRegistry jobRegistry) {
-		this.taskScheduler = taskScheduler;
-		this.runnableJobFactory = runnableJobFactory;
-		this.jobRegistry = jobRegistry;
-		this.triggers = Collections.unmodifiableCollection(triggers);
-		this.scheduledJobs = new ArrayList<>();
+    public DefaultScheduler(Collection<TriggerDescriptor> triggers,
+                            TaskScheduler taskScheduler,
+                            RunnableJobFactory runnableJobFactory,
+                            JobRegistry jobRegistry) {
+        this.taskScheduler = taskScheduler;
+        this.runnableJobFactory = runnableJobFactory;
+        this.jobRegistry = jobRegistry;
+        this.triggers = Collections.unmodifiableCollection(triggers);
+        this.scheduledJobs = new ArrayList<>();
 
-		this.started = new AtomicBoolean(false);
-	}
+        this.started = new AtomicBoolean(false);
+    }
 
-	@Override
-	public int start() {
-		if (!started.compareAndSet(false, true)) {
-			throw new IllegalStateException("Already started");
-		}
+    @Override
+    public int start() {
+        if (!started.compareAndSet(false, true)) {
+            throw new IllegalStateException("Already started");
+        }
 
-		if (triggers.isEmpty()) {
-			LOGGER.info("No triggers, exiting");
-			return 0;
-		}
+        if (triggers.isEmpty()) {
+            LOGGER.info("No triggers, exiting");
+            return 0;
+        }
 
-		List<String> badTriggers = triggers.stream()
-				.filter(t -> !jobRegistry.getAvailableJobs().contains(t.getJob()))
-				.map(t -> t.getJob() + ":" + t.getTrigger())
-				.collect(Collectors.toList());
+        String badTriggers = triggers.stream()
+                .filter(t -> !jobRegistry.getAvailableJobs().contains(t.getJob()))
+                .map(t -> t.getJob() + ":" + t.getTrigger())
+                .collect(Collectors.joining(", "));
 
-		if (badTriggers.size() > 0) {
-			throw new BootiqueException(1, "No job object(s) for trigger(s): " + badTriggers);
-		}
+        if (badTriggers != null && badTriggers.length() > 0) {
+            throw new BootiqueException(1, "No job object(s) for trigger(s): " + badTriggers);
+        }
 
-		triggers.stream().forEach(tc -> {
-			Job job = jobRegistry.getJob(tc.getJob());
-			String jobName = job.getMetadata().getName();
+        triggers.forEach(tc -> {
+            Job job = jobRegistry.getJob(tc.getJob());
+            String jobName = job.getMetadata().getName();
 
-			Function<Schedule, ScheduledFuture<?>> scheduler = (schedule) -> {
-				LOGGER.info(String.format("Will schedule '%s'.. (%s)", jobName, schedule.getDescription()));
-				return schedule(job, Collections.emptyMap(), schedule.getTrigger());
-			};
+            Function<Schedule, ScheduledFuture<?>> scheduler = (schedule) -> {
+                LOGGER.info(String.format("Will schedule '%s'.. (%s)", jobName, schedule.getDescription()));
+                return schedule(job, Collections.emptyMap(), schedule.getTrigger());
+            };
 
-			ScheduledJob scheduledJob = new DefaultScheduledJob(jobName, scheduler);
-			scheduledJob.schedule(createSchedule(tc));
-			scheduledJobs.add(scheduledJob);
-		});
+            ScheduledJob scheduledJob = new DefaultScheduledJob(jobName, scheduler);
+            scheduledJob.schedule(createSchedule(tc));
+            scheduledJobs.add(scheduledJob);
+        });
 
-		return triggers.size();
-	}
+        return triggers.size();
+    }
 
-	private Schedule createSchedule(TriggerDescriptor tc) {
+    private Schedule createSchedule(TriggerDescriptor tc) {
         String cron = tc.getCron();
         long fixedDelayMs = tc.getFixedDelayMs();
         long fixedRateMs = tc.getFixedRateMs();
@@ -102,81 +101,81 @@ public class DefaultScheduler implements Scheduler {
             return Schedule.fixedRate(fixedRateMs, initialDelayMs);
         }
 
-        throw new IllegalStateException(
-				"Trigger is misconfigured. Either of 'cron', 'fixedDelayMs', 'fixedRateMs' must be set.");
+        throw new BootiqueException(1, 
+                "Trigger is misconfigured. Either of 'cron', 'fixedDelayMs', 'fixedRateMs' must be set.");
     }
 
-	@Override
-	public boolean isStarted() {
-		return started.get();
-	}
+    @Override
+    public boolean isStarted() {
+        return started.get();
+    }
 
-	@Override
-	public Collection<TriggerDescriptor> getTriggers() {
-		return triggers;
-	}
+    @Override
+    public Collection<TriggerDescriptor> getTriggers() {
+        return triggers;
+    }
 
-	@Override
-	public Collection<ScheduledJob> getScheduledJobs() {
-		return Collections.unmodifiableCollection(scheduledJobs);
-	}
+    @Override
+    public Collection<ScheduledJob> getScheduledJobs() {
+        return Collections.unmodifiableCollection(scheduledJobs);
+    }
 
-	@Override
-	public JobFuture runOnce(String jobName) {
-		return runOnce(jobName, Collections.emptyMap());
-	}
+    @Override
+    public JobFuture runOnce(String jobName) {
+        return runOnce(jobName, Collections.emptyMap());
+    }
 
-	@Override
-	public JobFuture runOnce(String jobName, Map<String, Object> parameters) {
-		Optional<Job> jobOptional = findJobByName(jobName);
-		if (jobOptional.isPresent()) {
-			Job job = jobOptional.get();
-			return runOnce(job, parameters);
-		} else {
-			return invalidJobNameResult(jobName, parameters);
-		}
-	}
+    @Override
+    public JobFuture runOnce(String jobName, Map<String, Object> parameters) {
+        Optional<Job> jobOptional = findJobByName(jobName);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            return runOnce(job, parameters);
+        } else {
+            return invalidJobNameResult(jobName, parameters);
+        }
+    }
 
-	private Optional<Job> findJobByName(String jobName) {
-		Job job = jobRegistry.getJob(jobName);
-		return (job == null) ? Optional.empty() : Optional.of(job);
-	}
+    private Optional<Job> findJobByName(String jobName) {
+        Job job = jobRegistry.getJob(jobName);
+        return (job == null) ? Optional.empty() : Optional.of(job);
+    }
 
-	private JobFuture invalidJobNameResult(String jobName, Map<String, Object> parameters) {
-		return JobFuture.forJob(jobName)
-				.future(new ExpiredFuture())
-				.runnable(() -> JobResult.unknown(JobMetadata.build(jobName)))
-				.resultSupplier(() -> JobResult.failure(JobMetadata.build(jobName), "Invalid job name: " + jobName))
-				.build();
-	}
+    private JobFuture invalidJobNameResult(String jobName, Map<String, Object> parameters) {
+        return JobFuture.forJob(jobName)
+                .future(new ExpiredFuture())
+                .runnable(() -> JobResult.unknown(JobMetadata.build(jobName)))
+                .resultSupplier(() -> JobResult.failure(JobMetadata.build(jobName), "Invalid job name: " + jobName))
+                .build();
+    }
 
-	@Override
-	public JobFuture runOnce(Job job) {
-		return runOnce(job, Collections.emptyMap());
-	}
+    @Override
+    public JobFuture runOnce(Job job) {
+        return runOnce(job, Collections.emptyMap());
+    }
 
-	@Override
-	public JobFuture runOnce(Job job, Map<String, Object> parameters) {
-		return submit(job, parameters,
-				(rj, result) -> taskScheduler.schedule(() -> result[0] = rj.run(), new Date()));
-	}
+    @Override
+    public JobFuture runOnce(Job job, Map<String, Object> parameters) {
+        return submit(job, parameters,
+                (rj, result) -> taskScheduler.schedule(() -> result[0] = rj.run(), new Date()));
+    }
 
-	private ScheduledFuture<?> schedule(Job job, Map<String, Object> parameters, Trigger trigger) {
-		return submit(job, parameters,
-				(rj, result) -> taskScheduler.schedule(() -> result[0] = rj.run(), trigger));
-	}
+    private ScheduledFuture<?> schedule(Job job, Map<String, Object> parameters, Trigger trigger) {
+        return submit(job, parameters,
+                (rj, result) -> taskScheduler.schedule(() -> result[0] = rj.run(), trigger));
+    }
 
-	private JobFuture submit(Job job, Map<String, Object> parameters,
-							 BiFunction<RunnableJob, JobResult[], ScheduledFuture<?>> executor) {
+    private JobFuture submit(Job job, Map<String, Object> parameters,
+                             BiFunction<RunnableJob, JobResult[], ScheduledFuture<?>> executor) {
 
-		RunnableJob rj = runnableJobFactory.runnable(job, parameters);
-		JobResult[] result = new JobResult[1];
-		ScheduledFuture<?> jobFuture = executor.apply(rj, result);
+        RunnableJob rj = runnableJobFactory.runnable(job, parameters);
+        JobResult[] result = new JobResult[1];
+        ScheduledFuture<?> jobFuture = executor.apply(rj, result);
 
-		return JobFuture.forJob(job.getMetadata().getName())
-				.future(jobFuture)
-				.runnable(rj)
-				.resultSupplier(() -> result[0] != null ? result[0] : JobResult.unknown(job.getMetadata()))
-				.build();
-	}
+        return JobFuture.forJob(job.getMetadata().getName())
+                .future(jobFuture)
+                .runnable(rj)
+                .resultSupplier(() -> result[0] != null ? result[0] : JobResult.unknown(job.getMetadata()))
+                .build();
+    }
 }
