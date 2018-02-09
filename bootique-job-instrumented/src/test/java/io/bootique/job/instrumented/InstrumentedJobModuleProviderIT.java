40b5d22028d9a01b51d64c3bc3a7e93777290e2f
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 11:15:39 +0300

    Declare module dependencies bootique/bootique#203
    Dependency on bootique-curator removed, since this is optional dependency. Added tests for instrumented module.

--- bootique-job-instrumented/src/test/java/io/bootique/job/instrumented/InstrumentedJobModuleProviderIT.java
+++ bootique-job-instrumented/src/test/java/io/bootique/job/instrumented/InstrumentedJobModuleProviderIT.java
@@ -1,12 +1,28 @@
 package io.bootique.job.instrumented;
 
+import io.bootique.BQRuntime;
+import io.bootique.job.runtime.JobModule;
+import io.bootique.metrics.MetricsModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static com.google.common.collect.ImmutableList.of;
 
 public class InstrumentedJobModuleProviderIT {
 
-	@Test
-	public void testPresentInJar() {
-		BQModuleProviderChecker.testPresentInJar(InstrumentedJobModuleProvider.class);
-	}
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
+
+    @Test
+    public void testPresentInJar() {
+        BQModuleProviderChecker.testPresentInJar(InstrumentedJobModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new InstrumentedJobModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(JobModule.class, MetricsModule.class));
+    }
 }
