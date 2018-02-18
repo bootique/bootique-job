fc2889643c3770c70d55836e838f83d4d4d4fc9f
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:31:55 +0300

    Declare module dependencies bootique/bootique#203
    * new test API

--- bootique-job-instrumented/src/test/java/io/bootique/job/instrumented/InstrumentedJobModuleProviderIT.java
+++ bootique-job-instrumented/src/test/java/io/bootique/job/instrumented/InstrumentedJobModuleProviderIT.java
@@ -4,11 +4,10 @@ import io.bootique.BQRuntime;
 import io.bootique.job.runtime.JobModule;
 import io.bootique.metrics.MetricsModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class InstrumentedJobModuleProviderIT {
 
@@ -23,6 +22,6 @@ public class InstrumentedJobModuleProviderIT {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new InstrumentedJobModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(JobModule.class, MetricsModule.class));
+        BQRuntimeChecker.testModulesLoaded(bqRuntime, JobModule.class, MetricsModule.class);
     }
 }
