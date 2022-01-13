af312e390543a1e36dd6f29fc88c538a7600c585
Andrus Adamchik <andrus@objectstyle.com>
2022-01-13 08:52:15 +0200

    better logging for local lock

--- bootique-job/src/main/java/io/bootique/job/lock/LocalLockHandler.java
+++ bootique-job/src/main/java/io/bootique/job/lock/LocalLockHandler.java
@@ -19,60 +19,62 @@
 
 package io.bootique.job.lock;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.bootique.job.JobMetadata;
 import io.bootique.job.runnable.JobOutcome;
 import io.bootique.job.runnable.JobResult;
 import io.bootique.job.runnable.RunnableJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Cluster unaware {@link LockHandler}.
  */
 public class LocalLockHandler implements LockHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(LocalLockHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocalLockHandler.class);
 
-	private final ConcurrentMap<String, Lock> locks;
+    private final ConcurrentMap<String, Lock> locks;
 
-	public LocalLockHandler() {
-		this.locks = new ConcurrentHashMap<>();
-	}
+    public LocalLockHandler() {
+        this.locks = new ConcurrentHashMap<>();
+    }
 
-	@Override
-	public RunnableJob lockingJob(RunnableJob executable, JobMetadata metadata) {
+    @Override
+    public RunnableJob lockingJob(RunnableJob executable, JobMetadata metadata) {
 
-		return () -> {
-			String lockName = toLockName(metadata);
-			Lock lock = getLock(lockName);
+        return () -> {
+            String lockName = toLockName(metadata);
+            Lock lock = getLock(lockName);
 
-			LOGGER.info("Attempting to lock '{}'", lockName);
+            if (!lock.tryLock()) {
+                LOGGER.info("Skipping execution of '{}', another job instance owns the lock.", metadata.getName());
+                return new JobResult(
+                        metadata,
+                        JobOutcome.SKIPPED,
+                        null,
+                        "Skipping execution, another job instance owns the lock");
+            }
 
-			if (!lock.tryLock()) {
-				LOGGER.info("== Another job instance owns the lock. Skipping execution of '{}'", lockName);
-				return new JobResult(metadata, JobOutcome.SKIPPED, null,
-						"Another job instance owns the lock. Skipping execution");
-			}
+            LOGGER.info("Locked '{}'", metadata.getName());
 
-			try {
-				return executable.run();
-			} finally {
-				lock.unlock();
-			}
-		};
-	}
+            try {
+                return executable.run();
+            } finally {
+                lock.unlock();
+            }
+        };
+    }
 
-	private Lock getLock(String lockName) {
-		return locks.computeIfAbsent(lockName, k -> new ReentrantLock());
-	}
+    private Lock getLock(String lockName) {
+        return locks.computeIfAbsent(lockName, k -> new ReentrantLock());
+    }
 
-	private String toLockName(JobMetadata metadata) {
-		return metadata.getName();
-	}
+    private String toLockName(JobMetadata metadata) {
+        return metadata.getName();
+    }
 }
