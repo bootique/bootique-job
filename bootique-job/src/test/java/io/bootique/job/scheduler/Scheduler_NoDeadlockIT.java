fe0424cb85e325b2dfe203ecf5e8a3169ca005a8
Andrus Adamchik <andrus@objectstyle.com>
2022-06-04 11:10:39 +0200

    Jobs with dependencies and job groups cause deadlocks on pool starvation #102
    test to reproduce ... in disabled state

---
+++ bootique-job/src/test/java/io/bootique/job/scheduler/Scheduler_NoDeadlockIT.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.bootique.job.scheduler;
+
+import io.bootique.BQCoreModule;
+import io.bootique.BQRuntime;
+import io.bootique.Bootique;
+import io.bootique.job.BaseJob;
+import io.bootique.job.JobMetadata;
+import io.bootique.job.JobModule;
+import io.bootique.job.runnable.JobFuture;
+import io.bootique.job.runnable.JobOutcome;
+import io.bootique.job.runnable.JobResult;
+import io.bootique.junit5.BQApp;
+import io.bootique.junit5.BQTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@BQTest
+public class Scheduler_NoDeadlockIT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler_NoDeadlockIT.class);
+
+    @BQApp(skipRun = true)
+    final BQRuntime app = Bootique.app()
+            .autoLoadModules()
+            .module(b -> BQCoreModule.extend(b)
+                    .setProperty("bq.scheduler.threadPoolSize", "2")
+                    .setProperty("bq.jobs.j1.dependsOn[0]", "j2")
+                    .setProperty("bq.jobs.j1.dependsOn[1]", "j3")
+            )
+            .module(b -> JobModule.extend(b)
+                    .addJob(J1.class)
+                    .addJob(J2.class)
+                    .addJob(J3.class))
+            .createRuntime();
+
+    @Disabled("Until a fix is available per #102")
+    @RepeatedTest(3)
+    public void testAttemptDeadlock() {
+        Scheduler scheduler = app.getInstance(Scheduler.class);
+
+        List<JobFuture> futures = List.of(
+                scheduler.runOnce("j1"),
+                scheduler.runOnce("j1"));
+
+        for (JobFuture f : futures) {
+            JobResult r = f.get(1, TimeUnit.SECONDS);
+            assertEquals(JobOutcome.SUCCESS, r.getOutcome());
+        }
+    }
+
+    static final class J1 extends BaseJob {
+
+        public J1() {
+            super(JobMetadata.build(J1.class));
+        }
+
+        @Override
+        public JobResult run(Map<String, Object> params) {
+            LOGGER.info("1 in progress");
+            return JobResult.success(getMetadata());
+        }
+    }
+
+    static final class J2 extends BaseJob {
+
+        public J2() {
+            super(JobMetadata.build(J2.class));
+        }
+
+        @Override
+        public JobResult run(Map<String, Object> params) {
+            LOGGER.info("2 in progress");
+            return JobResult.success(getMetadata());
+        }
+    }
+
+    static final class J3 extends BaseJob {
+
+        public J3() {
+            super(JobMetadata.build(J3.class));
+        }
+
+        @Override
+        public JobResult run(Map<String, Object> params) {
+            LOGGER.info("3 in progress");
+            return JobResult.success(getMetadata());
+        }
+    }
+}
